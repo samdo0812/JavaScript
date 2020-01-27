0e7ec2b73808816add47f1b9ba374ede342bd101
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-01-27 23:02:38 +0900

    함수
    day5

---
+++ start_JS/day_05_02.js
@@ -0,0 +1,10 @@
+function get_member1() {
+	return 'kim';
+}
+
+function get_member2() {
+	return 'samdo'
+}
+
+alert(get_member1());
+alert(get_member2());
---
+++ start_JS/day_05_03.js
@@ -0,0 +1,7 @@
+function get_argument(argument) {
+	return argument;
+}
+
+alert(get_argument(1));
+
+alert(get_argument(2));
