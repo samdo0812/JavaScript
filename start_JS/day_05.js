3ebd31b3f7d75d3cebe235ca93ba23f014a95c2a
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-01-27 22:19:27 +0900

    함수
    day5

---
+++ start_JS/day_05.js
@@ -0,0 +1,11 @@
+function numbering(argument) {
+	i = 0;
+	while(i<10){
+		document.write(i);
+		i+=1;
+	}
+}
+
+numbering();
+numbering();
+numbering();
---
+++ start_JS/day_05_01.js
@@ -0,0 +1,8 @@
+function numbering(){
+    var i = 0;
+    while(i < 20){
+        document.write(i + "<br />");
+        i += 1;
+    }   
+}
+numbering();
