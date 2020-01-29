cdb03e56096b43e51ff5043013e1d0e0b7339bc1
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-01-29 23:05:49 +0900

    배열
    day5

--- start_JS/day_06_02.js
+++ start_JS/day_06_02.js
@@ -1,1 +1,21 @@
-day_06_02.js
+var li = ['a','b','c','d'];
+li.push('e');
+alert(li);
+
+var li = ['a','b','c','d'];
+li = li.concat(['e','f']);
+alert(li);
+
+var li = ['a','b','c','d'];
+li.unshift('z');
+alert(li);
+
+var li = ['a','b','c','d'];
+li.splice(2,0,'B');
+alert(li);
+/*
+array.splice(index, howmany, element);
+index : 배열의 위치
+howmany : index에서부터 제거 될 원소들의 수, 이 값이 0이면 어떠한 원소도 삭제되지 않는다.
+element : index+homany사이에 추가될 값
+*/
---
+++ start_JS/day_06_03.js
@@ -0,0 +1,15 @@
+var li = ['a','b','c','d'];
+li.shift();
+alert(li);
+
+var li = ['a','b','c','d'];
+li.pop();
+alert(li);
+
+var li = ['a','c','b','e','d'];
+li.sort();
+alert(li);
+
+var li = ['a','c','b','e','d'];
+li.reverse();
+alert(li);
