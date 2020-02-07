38a114ce14b405b4abc9bfea31a0eddbacf2df28
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-02-07 22:14:20 +0900

    함수지향-유효범위

--- start_JS/day_08_04.js
+++ start_JS/day_08_04.js
@@ -1,1 +1,16 @@
-day_08_04.js
+var MYAPP = {}
+MYAPP.calculator = {
+    'left' : null,
+    'right' : null
+}
+MYAPP.coordinate = {
+    'left' : null,
+    'right' : null
+}
+ 
+MYAPP.calculator.left = 10;
+MYAPP.calculator.right = 20;
+function sum(){
+    return MYAPP.calculator.left + MYAPP.calculator.right;
+}
+document.write(sum());
