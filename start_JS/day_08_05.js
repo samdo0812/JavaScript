d21a1a51998ac668e16e717a9a6973dccd94f8ce
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-02-10 20:52:39 +0900

    함수지향-유효범위

---
+++ start_JS/day_08_05.js
@@ -0,0 +1,14 @@
+for (var i = 0; i < 1; i++) {
+	var name = 'coding everybody';
+}
+alert(name);
+
+/*
+java에서는 불가능
+
+for (var i = 0; i < 1; i++) {
+	String name = 'coding everybody';
+}
+alert(name);
+
+*/
