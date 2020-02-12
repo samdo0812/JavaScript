9d15c8fedc43ec0b6c53cdda1379e3f9375714bf
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-02-12 22:01:57 +0900

    함수지향-값으로서의 함수와 콜백

---
+++ start_JS/day_09.js
@@ -0,0 +1,14 @@
+function cal(func, num) {
+	return func(num)
+}
+
+function increase(num){
+	return num+1
+}
+
+function decrease(num){
+	return num-1
+}
+
+alert(cal(increase, 1));
+alert(cal(decrease, 1));
