68a5d91f296f98a4f7024af7fe82904e159fd9b7
samdo0812 <39455589+samdo0812@users.noreply.github.com>
2020-02-13 21:43:36 +0900

    햠수지향-값으로서의 함수와 콜백

---
+++ start_JS/day_09_01.js
@@ -0,0 +1,9 @@
+function cal(mode){
+    var funcs = {
+        'plus' : function(left, right){return left + right},
+        'minus' : function(left, right){return left - right}
+    }
+    return funcs[mode];
+}
+alert(cal('plus')(2,1));
+alert(cal('minus')(2,1));
